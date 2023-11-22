aff4b7027bdfc3f66907af1d313351a82299ac06
Timereader <53362690+wongjushao@users.noreply.github.com>
2023-11-22 22:10:14 +0800

    modified:   resources/views/form.blade.php 	modified:   resources/views/layouts/app.blade.php

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -2,50 +2,42 @@
 
 @section('title',isset($task) ? 'Edit Task':'Add Task')
 
-@section('styles')
-    <style>
-        .error-message{
-            color: red;
-            font-size: 0.8rem;
-        }
-    </style>
-@endsection
-
 @section('content')
     <form method="POST" action="{{isset($task)? route('tasks.update',['task'=>$task->id]) : route('tasks.store')}}">
     @csrf 
     @isset($task)
         @method('PUT')        
     @endisset
-    <div>
+    <div class="mb-4">
         <label for="title">Title</label>
-        <input type="text" name="title" id="title" value="{{$task->title ?? old('title')}}">
+        <input type="text" name="title" id="title" @class(['border-red-500'=> $errors->has('title')])value="{{$task->title ?? old('title')}}">
         @error('title')
             <p class="error-message">{{$message}}</p>
         @enderror
     </div>
-    <div>
+    <div class="mb-4">
         <label for="description">Description</label>
-        <textarea name="description" id="description"rows="5">{{$task->description ?? old('description')}}</textarea>
+        <textarea name="description" id="description" @class(['border-red-500'=> $errors->has('title')]) rows="5">{{$task->description ?? old('description')}}</textarea>
         @error('description')
         <p class="error-message">{{$message}}</p>
         @enderror
     </div>
-    <div>
+    <div class="mb-4">
         <label for="long_description">Long_Description</label>
-        <textarea name="long_description" id="long_description"rows="10">{{ $task->long_description ?? old('long_description')}}</textarea>
+        <textarea name="long_description" id="long_description" @class(['border-red-500'=> $errors->has('title')])  rows="10">{{ $task->long_description ?? old('long_description')}}</textarea>
         @error('long_description')
         <p class="error-message">{{$message}}</p>
         @enderror
     </div>
-    <div>
-        <button type="submit">
+    <div class="flex gap-2 items-center" >
+        <button type="submit" class="btn">
            @isset($task)
                Update Task
                @else
                Add Task
            @endisset 
         </button>
+        <a href="{{route('tasks.index')}}" class="link">Cancel</a>
     </div>
     </form>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="//unpkg.com/alpinejs" defer></script>
     <style type="text/tailwindcss">
     .btn{
-        @apply rounded-md px-2 py-1 text-center font-medium shadow-sm text-slate-700 ring-1 ring-slate-700/10 hover:bg-slate-50 
+        @apply rounded-md px-2 py-1 text-center font-medium shadow-sm text-slate-700 ring-1 ring-slate-700/50 hover:bg-slate-50 
     }
     .link{
         @apply font-medium text-gray-700 underline decoration-pink-500
+    }
+    label{
+        @apply block uppercase text-slate-700 mb-2
+    }
+    input,textarea{
+        @apply shadow-sm appearance-none border w-full py-2 px-3 text-slate-700 leading-tight 
+    }
+    .error-message{
+        @apply text-red-500 text-sm
+
     }
     </style>
     @vite('resources/css/app.css')
@@ -18,10 +29,21 @@
 </head>
 <body class="contanier mx-auto mt-10 mb-10 max-w-lg" >
     <h1 class="text-2xl mb-4">@yield('title')</h1>
-    <div>
-        @if (session()->has('success'))
-            <div>{{session('success')}}</div>
-        @endif
+    <div x-data="{flash:true}">
+    <div x-show="flash">
+        {{-- @if (session()->has('success')) --}}
+            {{-- <div>{{session('success')}}</div> --}}
+            <div class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700" role="alert">
+                <strong class="font-bold">Success!㊗️</strong>
+                <div> flash message</div>
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 cursor-pointer" @click="flash = false" >
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>                        
+                    </svg>
+                </span>
+            </div>
+        </div>
+        {{-- @endif --}}
         @yield('content')
     </div>
 </body>
